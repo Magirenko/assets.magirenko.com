2ee04e8093257a6ce5b2b27d57b3e7b0405bea1d
Migue Magic <miguelitoeremenko@gmail.com>
2025-01-26 10:19:12 -0400

    Add files via upload

--- js/header_and_footer.js
+++ js/header_and_footer.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     var req1 = new XMLHttpRequest();
-req1.open("GET", "/blocks/header.html", false);
+req1.open("GET", "https://assets.magirenko.com/blocks/header.html", false);
 req1.send();
 var header = req1.responseText;
 
 var req2 = new XMLHttpRequest();
-req2.open("GET", "/blocks/footer.html", false);
+req2.open("GET", "https://assets.magirenko.com/blocks/footer.html", false);
 req2.send();
 var footer = req2.responseText;
 
