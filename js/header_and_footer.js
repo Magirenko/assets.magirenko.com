d21f46c33ed696e4f1f1bc8294fa2fbb6bb5c3b4
Migue Magic <miguelitoeremenko@gmail.com>
2025-02-24 15:45:06 +0000

    Update header_and_footer.js

--- js/header_and_footer.js
+++ js/header_and_footer.js
@@ -1,17 +1,17 @@
 function placehf() {
-    document.addEventListener("DOMContentLoaded", (event) => {
-        var req1 = new XMLHttpRequest();
-        req1.open("GET", "https://assets.magirenko.com/blocks/header.html", false);
-        req1.send();
-        var header = req1.responseText;
+    var req1 = new XMLHttpRequest();
+    req1.open("GET", "https://assets.magirenko.com/blocks/header.html", false);
+    req1.send();
+    var header = req1.responseText;
+    
+    var req2 = new XMLHttpRequest();
+    req2.open("GET", "https://assets.magirenko.com/blocks/footer.html", false);
+    req2.send();
+    var footer = req2.responseText;
         
-        var req2 = new XMLHttpRequest();
-        req2.open("GET", "https://assets.magirenko.com/blocks/footer.html", false);
-        req2.send();
-        var footer = req2.responseText;
-        
-        document.getElementById("page").innerHTML = header + document.getElementById("page").innerHTML + footer;
-    })
+    document.getElementById("page").innerHTML = header + document.getElementById("page").innerHTML + footer;
 }
 
-placehf()
+document.addEventListener("DOMContentLoaded", (event) => {
+    placehf()
+});
