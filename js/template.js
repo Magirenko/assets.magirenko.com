761517e8ea04e828a1b58f2f080470a2c3f765f2
Migue Magic <miguelitoeremenko@gmail.com>
2025-02-24 15:43:30 +0000

    Update template.js

--- js/template.js
+++ js/template.js
@@ -1,12 +1,11 @@
 function PlaceTemp(uri) {
-   document.addEventListener("DOMContentLoaded", (event) => {
         var PlaceTemplate = document.getElementById("PlaceTemplate");
         var val = uri;
         var req = new XMLHttpRequest();
         req.open("GET", val, false);
         req.send();
         document.body.innerHTML = req.responseText;
-   });
 }
-
- PlaceTemp(PlaceTemplate.getAttribute("template"));
+document.addEventListener("DOMContentLoaded", (event) => {
+    PlaceTemp(PlaceTemplate.getAttribute("template"));
+});
